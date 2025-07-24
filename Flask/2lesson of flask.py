1d6c03ee18240def30b2ca677fe06ea2f6c58354
kazakcoder <pythonacc822@gmail.com>
2025-07-25 00:42:33 +0500

    Update 2lesson of flask.py

--- Flask/2lesson of flask.py
+++ Flask/2lesson of flask.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask,render_template#Flask және render_template алу
 
-app = Flask(__name__)
+app = Flask(__name__)#интализация Flask
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html")#index.html саитка шығару Templates Фаиылында
 
 if __name__=="__main__":
     app.run(debug=True)
