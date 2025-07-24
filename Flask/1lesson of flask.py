196733dcb465887fcf99d1d147a4de5a5eb72942
kazakcoder <pythonacc822@gmail.com>
2025-07-25 00:40:33 +0500

    Update 1lesson of flask.py

--- Flask/1lesson of flask.py
+++ Flask/1lesson of flask.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask #Flask кітапханасын алу
 
-app = Flask(__name__)
-@app.route('/')
+app = Flask(__name__)#Flask интализация
+@app.route('/home')#саитка сылка роуте жасау мысалы:http://127.0.0.1:5000/home
 def home():
-  return "hello, World"
+  return "hello, World"#жазу шығару веб саита
 if __name__=="__main__":
   app.run(debug=True)
